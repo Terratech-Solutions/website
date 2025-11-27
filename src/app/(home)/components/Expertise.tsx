04138054dbfd106c21d7a05ff67b7cf2aee89381
Valentyn Yefimov <valentine.efimov@gmail.com>
2025-11-27 22:09:26 +0100

    feat: update image format from PNG to JPG for consistency in Expertise component

--- src/app/(home)/components/Expertise.tsx
+++ src/app/(home)/components/Expertise.tsx
@@ -7,7 +7,7 @@ const Expertise = () => {
       <h2 className="text-[44px] max-sm:text-[35px]">{page.expertise.title}</h2>
       <p className="mt-2.5 font-light">{page.expertise.description}</p>
       <div className="relative w-full h-[445px] max-xl:h-[350px] max-lg:h-[300px] max-md:h-[200px] max-sm:h-[150px] mt-11">
-        <Image src="/home/expertise/road.png" alt="Road preview" fill priority />
+        <Image src="/home/expertise/road.jpg" alt="Road preview" fill priority />
       </div>
     </section>
   );
