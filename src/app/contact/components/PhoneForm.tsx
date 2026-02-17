7f1f28ea028379c4b4912e757c2d8193e28fcc38
John Lewy <lewyjohn2014@gmail.com>
2026-02-17 16:18:55 -0500

    fix: JSX parse error

--- src/app/contact/components/PhoneForm.tsx
+++ src/app/contact/components/PhoneForm.tsx
@@ -1,5 +1,7 @@
 'use client';
+}
 
+export default PhoneForm;
 import { phoneFormData } from '@/data/contact.json';
 import { zodResolver } from '@hookform/resolvers/zod';
 import Image from 'next/image';
